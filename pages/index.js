4ba8a912c134c2ee6e75af9d0e45167e4044a99d
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2022-05-22 18:17:14 +0300

    Add logo

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="h-screen">
+    <div className="h-screen flex flex-col justify-between p-6">
       <Head>
         <title>Krypto</title>
         <meta name="description" content="Track crypto market with Krypto!" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Krypto</main>
+      <header className="relative w-32 h-8">
+        <Image src="/logo.svg" layout="fill" alt="logo" objectFit="content" />
+      </header>
+      <main></main>
     </div>
   );
 }
