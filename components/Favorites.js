3a1c944397de68798a662076466090d3b243801a
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2022-05-24 10:52:48 +0300

    Add empty favorites warning

--- components/Favorites.js
+++ components/Favorites.js
@@ -12,33 +12,37 @@ export const Favorites = () => {
         <SelectedStarIcon />
         <h1 className="font-bold">Favorites</h1>
       </div>
-      <div className="w-full text-sm flex justify-center border-2 border-solid border-black rounded-lg p-1">
-        <table className="table-auto w-full text-center border-collapse">
-          <thead>
-            <tr>
-              <th></th>
-              <th></th>
-              <th>Coin</th>
-              <th>Price</th>
-              <th>24h</th>
-              <th>Last 7 Days</th>
-            </tr>
-          </thead>
-          <tbody>
-            {favorites.map((f) => (
-              <FavoritedCrypto
-                id={f.id}
-                key={f.id}
-                icon={f.icon}
-                price={f.price}
-                oneDayChange={f.oneDayChange}
-                name={f.name}
-                sparkline={f.sparkline}
-              />
-            ))}
-          </tbody>
-        </table>
-      </div>
+      {favorites.length === 0 ? (
+        <p className="text-center">{`You don't have any favorite`}</p>
+      ) : (
+        <div className="w-full text-sm flex justify-center border-2 border-solid border-black rounded-lg p-1">
+          <table className="table-auto w-full text-center border-collapse">
+            <thead>
+              <tr>
+                <th></th>
+                <th></th>
+                <th>Coin</th>
+                <th>Price</th>
+                <th>24h</th>
+                <th>Last 7 Days</th>
+              </tr>
+            </thead>
+            <tbody>
+              {favorites.map((f) => (
+                <FavoritedCrypto
+                  id={f.id}
+                  key={f.id}
+                  icon={f.icon}
+                  price={f.price}
+                  oneDayChange={f.oneDayChange}
+                  name={f.name}
+                  sparkline={f.sparkline}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
